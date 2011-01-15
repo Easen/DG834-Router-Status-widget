51cbe541318e76489cf564e8e10edefc666e587e
Marc Easen <marc@easen.co.uk>
2011-01-15 02:39:44 +0000

    Updated to support the externals dir

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-cp -R ruby widget/DG834\ Adsl\ Status.dcproj/project/widget.wdgt/
+mkdir -p  widget/DG834\ Adsl\ Status.dcproj/project/widget.wdgt/ruby/
+cp ruby/get-router-status.rb widget/DG834\ Adsl\ Status.dcproj/project/widget.wdgt/ruby/
+cp -R externals/json/lib/* widget/DG834\ Adsl\ Status.dcproj/project/widget.wdgt/ruby/
